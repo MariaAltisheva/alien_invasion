e4cbb376e41b80aac674f02df784c6f54bb91e46
MariaAltysheva <altysheva_maria@mail.ru>
2022-07-07 21:51:31 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,45 @@
+class Settings:
+    """Класс для хранения всех настроек игры Alien Invasion"""
+    def __init__(self):
+        """Инициализирует статические настройки игры"""
+        #Параметры экрана
+        self.screen_width =1200
+        self.screen_height = 800
+        self.bg_color = (255,255, 255)
+        #Настройки корабля
+        self.ship_speed = 5 #1.5
+        self.ship_limit = 3
+
+        self.bullet_speed = 3
+        self.bullet_width = 15
+        self.bullet_height = 30
+        self.bullet_color = (60,60, 60)
+        self.bullet_allowed = 5 #3
+
+        #Настройки пришельцев
+        self.alien_speed = 3 #1.50
+        self.fleet_drop_speed = 10 #10
+
+        # Темп ускорения игры
+        self.speedup_scale = 5.1
+        #Темп роста стоимости пришельцев
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.0
+        # fleet_direction = 1 обозначает движение вправо, а -1 - влево
+        self.fleet_direction = 1
+        #подсчет очков
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """увеличивает настройки скорости и стоимость пришгельцев"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
